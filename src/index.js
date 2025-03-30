01af70b6f480997411e1d89c4dfc785d2fb6ae3b
whitl33 <whitneyslee@gmail.com>
2025-03-30 17:10:53 -0400

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@ function updateWeather(response){
 let temperatureElement = document.querySelector("#temperature");
 let temperature = response.data.temperature.current;
 let cityElement = document.querySelector("#city");
-cityElement.innerHTML = response.data.city;
-temperatureElement.innerHTML = Math.round(temperature); 
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windSpeedElement = document.querySelector("#wind-speed");
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time * 1000);
 
+cityElement.innerHTML = response.data.city;
+timeElement.innerHTML = formatDate(date);
+humidityElement.innerHTML =`${response.data.temperature.humidity}%`;
+windSpeedElement.innerHTML = `${response.data.wind.speed}km/H`;
+temperatureElement.innerHTML = Math.round(temperature); 
+descriptionElement.innerHTML = response.data.condition.description;
+}
+function formatDate(date){
+let minutes = date.getMinutes();
+let hours = date.getHours();
+let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday", "Saturday"];
+let day = days[date.getDay()];
+if (minutes < 10){
+minutes = `0${minutes}`;
+}
+return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city){
