d56af66b31a4e8d276a94720e9d4366bc8856db9
whitl33 <whitneyslee@gmail.com>
2025-03-30 09:10:32 -0400

    Added in the search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-function handleSearchSubmit(event){
-   event.preventDefault();
-
-
-let searchInput = document.querySelector("#search-form-input");
+function updateWeather(response){
+let temperatureElement = document.querySelector("#temperature");
+let temperature = response.data.temperature.current;
 let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = Math.round(temperature); 
+
+}
+
+function searchCity(city){
+let apiKey = "o55206ae8847051e1ff334btaf13bafe";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateWeather);
+}
+
+function handleSearchSubmit(event){
+event.preventDefault();
+let searchInput = document.querySelector("#search-form-input");
+
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
+
+searchCity("San Francisco");
